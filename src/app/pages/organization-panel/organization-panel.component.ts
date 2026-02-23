982ceca1f639f61b7d86d1afa0ff5b4bfc2fa4ab
Shital24650 <shitalparab2605@gmail.com>
2026-02-23 14:12:14 +0530

    Create organization-panel.component.ts file with Organization Panel component

---
+++ src/app/pages/organization-panel/organization-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-panel',
+  templateUrl: './organization-panel.component.html',
+  styleUrls: ['./organization-panel.component.css']
+})
+export class OrganizationPanelComponent {
+  constructor() {}
+}
