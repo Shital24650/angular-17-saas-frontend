329e44d4abea8a8ba9908b3389d8649f9187a276
Shital24650 <shitalparab2605@gmail.com>
2026-02-23 14:12:35 +0530

    Create startup panel component

---
+++ src/app/pages/startup-panel/startup-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startup-panel',
+  templateUrl: './startup-panel.component.html',
+  styleUrls: ['./startup-panel.component.css']
+})
+export class StartupPanelComponent {
+  title = 'Startup Panel';
+
+  constructor() {}
+}
