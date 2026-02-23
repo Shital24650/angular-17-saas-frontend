32154971de1af1d98705b08f262b54b305e5869f
Shital24650 <shitalparab2605@gmail.com>
2026-02-23 14:12:24 +0530

    Create corporate-panel.component.ts file for corporate panel component

---
+++ src/app/pages/corporate-panel/corporate-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corporate-panel',
+  templateUrl: './corporate-panel.component.html',
+  styleUrls: ['./corporate-panel.component.scss']
+})
+export class CorporatePanelComponent {
+  // You can add properties and methods here for your component logic
+}
